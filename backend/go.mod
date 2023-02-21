da288de0cafb705edd3673609002f137ca8e39a3
Yunkon Kim <hermitkim1@gmail.com>
2023-02-21 15:33:13 +0900

    Handle multiple clients

--- backend/go.mod
+++ backend/go.mod
@@ -1,5 +1,5 @@
-module github.com/yunkon-kim/go-reactjs-playground/backend
+module github.com/yunkon-kim/go-reactjs-playground
 
 go 1.19
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require github.com/gorilla/websocket v1.5.0
--- backend/main.go
+++ backend/main.go
@@ -4,20 +4,32 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/yunkon-kim/go-reactjs-playground/backend/pkg/websocket"
+	"github.com/yunkon-kim/go-reactjs-playground/pkg/websocket"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		fmt.Fprintf(w, "%+V\n", err)
+		fmt.Fprintf(w, "%+v\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
 }
 
 func main() {

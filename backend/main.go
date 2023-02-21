e1ff738e2cc7d09d1003981bc2fd64c5d430a039
Yunkon Kim <hermitkim1@gmail.com>
2023-02-21 14:41:18 +0900

    Initial commit

---
+++ backend/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/yunkon-kim/go-reactjs-playground/pkg/websocket"
+)
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+V\n", err)
+	}
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/ws", serveWs)
+}
+
+func main() {
+	fmt.Println("Distributed Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,67 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// App.js
+import React, { Component } from "react";
+import "./App.css";
+import { connect, sendMsg } from "./api";
+import Header from './components/Header/Header';
+import ChatHistory from "./components/ChatHistory";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: []
+    }
+  }
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
+  }
+}
+
+export default App;
